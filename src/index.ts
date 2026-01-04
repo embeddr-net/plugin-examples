f7c0a177925332d9eaf9e3bc2130a15b59f87b99
Nynxz <contact@nynxz.com>
2026-01-05 04:59:29 +1000

    add types

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { LayerEditorPlugin } from "../examples/LayerEditorPlugin";
+export { ArcadePlugin } from "../examples/embeddr_arcade/ArcadePlugin";
+export { PetPanelPlugin } from "../examples/PetPanelPlugin";
+export { GenerateButtonPlugin } from "../examples/GenerateButtonPlugin";
